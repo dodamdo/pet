fe2ad614c0bc144031c260f3dae8c0a6e6a87010
ANY <it_hing159@naver.com>
2024-10-23 22:32:30 +0900

    입력 시간 편하게 30분단위로 수정요청 완료

--- src/main/java/com/the/pet/config/SecurityConfig.java
+++ src/main/java/com/the/pet/config/SecurityConfig.java
@@ -1,17 +1,13 @@
 package com.the.pet.config;
 
 import com.the.pet.security.JwtTokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,7 +36,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login") // 커스텀 로그인 페이지 경로
-                        .defaultSuccessUrl("/priceList", true) // 로그인 성공 시 이동할 페이지
+                        .defaultSuccessUrl("/calendar", true) // 로그인 성공 시 이동할 페이지
                         .permitAll()
                 )
                 .logout(logout -> logout.permitAll());
